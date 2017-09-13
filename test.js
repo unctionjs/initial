1eef9d4c0385661f2890275e79c7569a2614d5b2
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-09-13 06:10:16 -0700

    node really doesn't like non index files

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import initial from "./source.js"
+import initial from "./"
 
 test(({same, end}) => {
   same(
