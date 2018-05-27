b312c48806eecfa2fc087c7641743c16eac7617f
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:31 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-magic-numbers */
-import dropLast from "@unction/droplast"
-
-export default function initial (orderedList: ArrayType | string): ArrayType | string {
-  return dropLast(1)(orderedList)
+import dropLast from "@unction/droplast";
+export default function initial(orderedList) {
+  return dropLast(1)(orderedList);
 }
