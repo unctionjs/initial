8f1cf23fa7c7fbbbfa9cca226cde69f3a0219b45
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-29 09:49:49 -0800

    Removing more bad types

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-magic-numbers */
 import dropLast from "@unction/droplast";
 
-export default function initial<A> (orderedList: OrderedArray<A> | Set<A> | RecordType<unknown, A> | string): OrderedArray<A> | Set<A> | RecordType<unknown, A> | string {
+export default function initial<A> (orderedList: OrderedArray<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string): OrderedArray<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string {
   return dropLast(1)(orderedList);
 }
